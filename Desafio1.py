e229dc765271b77e56b2ee5273f8d28f781805b8
Isane Maria Wocsuk Marques <100165323+prof-isane@users.noreply.github.com>
2026-03-17 08:26:37 -0300

    Create Desafio1.py

---
+++ Desafio1.py
@@ -0,0 +1,13 @@
+# Solicita os números ao usuário
+
+n1 = float(input('Informe o primeiro número: '))
+
+n2 = float(input('Informe o segundo número: '))
+
+# Realiza a soma
+
+soma = n1 + n2
+
+# Exibe o resultado
+
+print('A soma entre', n1, 'e', n2, 'é:', soma)
